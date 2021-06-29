60f2ea79686fe9dea246075f497a0f2726e0597a
nao0725 <aqua.725.times@gmail.com>
2021-06-29 08:16:35 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  
+  def hello
+    # render html:"hello,world!!"
+    render html:"¡Hola,mundo!"
+  end
+  
+  def goodbye
+    render html:"goobye,world!!"
+  end
+end
